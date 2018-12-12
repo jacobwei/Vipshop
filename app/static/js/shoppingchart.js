5475a11126f38455385e270e6b11bb4e4e1ec248
qqqcm <rnsymqlm@qq.com>
2018-12-12 19:10:44 +0800

    购物车count完成，剩下结算

--- app/static/js/shoppingchart.js
+++ app/static/js/shoppingchart.js
@@ -1,41 +1,73 @@
 var shopCar = (function () {
-    var $table = document.querySelector('.commodity');
-    var $count = document.querySelector('#count');
-    var count = $count.value;
     return {
         init() {
-            this.event();
+            this.$table = document.querySelector('.commodity');
             this.getData();
+            this.$count = document.querySelector('#count');
+            this.
+            this.count = this.$count.value ;
+            this.event();
         },
         event() {
             var _this = this;
-            $table.onclick = function(e){
+            this.$count.oninput = function (e) {
+                e = e || window.event;
+                var target =  e.target || e.srcElement;
+                var index = target.parentNode.parentNode.parentNode.index;                  
+                console.log(index);
+                if(target.id === 'count') {
+                    var data = _this.data[index];
+                    data.count = Number(target.value);
+                    localStorage.shopList = JSON.stringify(_this.data);
+                    target.parentNode.parentNode.parentNode.remove();
+                    _this.insertData(_this.data);
+                } 
+            }
+            this.$table.onclick = function(e){
                 e = e || window.event;
                 var target = e.target || e.srcElement;
                 var deltr = target.parentNode.parentNode;
                 if(target.id == 'del'){
+                    _this.data.splice(deltr.index, 1);
                     deltr.remove();
+                    localStorage.shopList = JSON.stringify(_this.data);
+
                 }else if(target.id == "add"){
-                    count = target.previousElementSibling.value;
-                    count++;                                            
-                    target.previousElementSibling.value = count;                                           
+                    _this.count = target.previousElementSibling.value;
+                    _this.count++;                                            
+                    // target.previousElementSibling.value = _this.count;
+                    var index = target.parentNode.parentNode.parentNode.index;
+                    var data = _this.data[index];
+                    data.count = _this.count;
+                    localStorage.shopList = JSON.stringify(_this.data);
+                    target.parentNode.parentNode.parentNode.remove();
+                    _this.insertData(_this.data);                                           
                 }else if(target.id == "sub"){
-                    count = target.nextElementSibling.value;
-                    count--;
-                    if(count <= 1){
-                        count = 1;
+                    _this.count = target.nextElementSibling.value;
+                    _this.count--;
+                    if(_this.count <= 1){
+                        _this.count = 1;
                     }
-                    target.nextElementSibling.value = count;
+                    // target.nextElementSibling.value = _this.count;
+                    var index = target.parentNode.parentNode.parentNode.index;
+                    var data = _this.data[index];
+                    data.count = _this.count;
+                    localStorage.shopList = JSON.stringify(_this.data);
+                    target.parentNode.parentNode.parentNode.remove();
+                    _this.insertData(_this.data);                                           
+
                 }
             }
         },
         getData() {
             var shopList = localStorage.shopList || '[]';
             shopList = JSON.parse(shopList);
+            this.data = shopList;
             console.log(shopList);
             this.insertData(shopList);
         },
         insertData(data) {
+            this.$table.innerHTML = '';
             data.forEach((item, index) => {
                 var $tr = document.createElement('tr');
                 $tr.setAttribute('class', 'goods');
@@ -65,8 +97,8 @@ var shopCar = (function () {
         <td>
 <!-- 删除按钮 --> <a id="del" style="cursor:pointer;">删除</a>                  
         </td>
-                `
-                $table.appendChild($tr);
+                `;
+                this.$table.appendChild($tr);
             })
         }
     }
