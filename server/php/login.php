138a9ef4725453beef42642c7d0803b7cddc8c88
qqqcm <rnsymqlm@qq.com>
2018-12-13 14:53:16 +0800

    数据同步下

--- server/php/login.php
+++ server/php/login.php
@@ -13,7 +13,7 @@
         else  */ 
         {  
 
-            $coon = new mysqli('10.36.141.211', 'root', '', 'vipshop_userinfo', 3306);
+            $coon = new mysqli('localhost', 'root', '', 'vipshop_userinfo', 3306);
             $sql = "SELECT username,userpwd FROM userinfo where username = '$user' and userpwd = '$psw'";  
             $coon->query("SET CHARACTER SET 'utf8'");//读库   
             $coon->query("SET NAMES 'utf8'");//写库 
